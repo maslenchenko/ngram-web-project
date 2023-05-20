bfffc9f4dabf72d51f8efdb0e1b10266eb8c549a
maslenchenko <o.maslenchenko@ucu.edu.ua>
2023-05-20 16:50:04 +0300

    words to predict now as numerical input

--- src/components/contextwords.js
+++ src/components/contextwords.js
@@ -1,27 +1,24 @@
-import React, { useState } from 'react';
-import '../styles/contextwords.css';
+import React, { useState } from "react";
+import "../styles/contextwords.css";
 
 function ContextWords() {
   const [range, setRange] = useState(1);
 
   const handleRangeChange = (event) => {
-    setRange(event.target.value);
-  };
-
-  const renderOptions = () => {
-    const options = [];
-    for (let i = 1; i <= 100; i++) {
-      options.push(<option key={i} value={i}>{i}</option>);
-    }
-    return options;
+    setRange(Number(event.target.value));
   };
 
   return (
-    <div className='context-container'>
-        <p className='context-title'>Words to predict</p>
-      <select className="context-list" value={range} onChange={handleRangeChange}>
-        {renderOptions()}
-      </select>
+    <div className="context-container">
+      <p className="context-title">Words to predict</p>
+      <input
+        type="number"
+        className="context-input"
+        value={range}
+        onChange={handleRangeChange}
+        min={1}
+        max={100000}
+      />
     </div>
   );
 }
