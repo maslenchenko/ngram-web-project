720458e963eae5188080674d767956a24431239c
Yaryna_Fialko <yaryna.fialko@ucu.edu.ua>
2023-05-20 18:06:30 +0300

    minor flexibility fixes

--- src/components/team-member.js
+++ src/components/team-member.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/team.css';
 
 
-function Student({name, description, email, student=true}) {
+function TeamMember({name, description, email, student = true}) {
     return (
         <div className="team-member">
-            <img src="https://via.placeholder.com/244x173" alt="team member" className='member-img' />
+            <img src="https://via.placeholder.com/244x173" alt="team member" className='member-img'/>
             <p className='member-name'>{name}</p>
-            {student ? <p className="student-description">{description}</p> : 
-            <p className="mentor-description">{description}</p> }
+            {student ? <p className="student-description">{description}</p> :
+                <p className="mentor-description">{description}</p>}
             <p className='member-email'>{email}</p>
         </div>
-    );};
+    );
+};
 
-
-// export default Mentor;
-export default Student;
+export default TeamMember;
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import Student from "../components/team-member.js";
-import '../styles/team.css';
 import {mentorName, studentName1, studentName2, studentName3, studentName4} from "../components/team-strings.js";
 import {mentorDescription, studentDescription1, studentDescription2, studentDescription3, studentDescription4 } from '../components/team-strings.js';
 import {mentorEmail, studentEmail1, studentEmail2, studentEmail3, studentEmail4} from "../components/team-strings.js";
+import '../styles/team.css';
+import TeamMember from "../components/team-member.js";
 
 function Team() {
   return (
     <div class='team-main'>
       <h1 className='team-title'>Let's get aquainted</h1>
       <div className='mentor'>
-        <Student name={mentorName}  description={mentorDescription} email={mentorEmail} student={false}/>
+        <TeamMember name={mentorName}  description={mentorDescription} email={mentorEmail} student={false}/>
       </div>
       <div className='students'>
-        <Student name={studentName1} description={studentDescription1} email={studentEmail1}/>
-        <Student name={studentName2} description={studentDescription2} email={studentEmail2}/>
-        <Student name={studentName3} description={studentDescription3} email={studentEmail3}/>
-        <Student name={studentName4} description={studentDescription4} email={studentEmail4}/>
+        <TeamMember name={studentName1} description={studentDescription1} email={studentEmail1}/>
+        <TeamMember name={studentName2} description={studentDescription2} email={studentEmail2}/>
+        <TeamMember name={studentName3} description={studentDescription3} email={studentEmail3}/>
+        <TeamMember name={studentName4} description={studentDescription4} email={studentEmail4}/>
       </div>
     </div>  
   )
